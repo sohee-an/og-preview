edac179ee98468c40de70445461020171f91e580
sohee-an <soan32@hanmail.net>
2025-10-05 18:52:15 +0900

    fix: header 부분 id 추가, 로직 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
 
-const HEADER_MENU = [{ id: 1, title: "OG 생성", url: "/go" }];
+const HEADER_MENU = [{ id: 1, title: "OG 생성", url: "/og" }];
 
 function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-10 p-4 border-b flex justify-between ">
       <Link href={"/"}>OG PREVIEW</Link>
       <div className="border border-blue-400 p-1.5 text-blue-400">
-        {HEADER_MENU.map((menu) => {
-          return <Link href={menu.url}>{menu.title}</Link>;
+        {HEADER_MENU.map(({ id, title, url }) => {
+          return (
+            <Link key={id} href={url}>
+              {title}
+            </Link>
+          );
         })}
       </div>
     </header>
