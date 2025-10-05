6e5b5cbddbf1430d5c2e684ab103bc0976ba8652
sohee-an <soan32@hanmail.net>
2025-10-05 18:34:07 +0900

    feat: og

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
+const HEADER_MENU = [{ id: 1, title: "OG 생성", url: "/go" }];
+
 function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 z-10 p-4 border-b ">
-      OG PREVIEW
+    <header className="fixed top-0 left-0 right-0 z-10 p-4 border-b flex justify-between ">
+      <Link href={"/"}>OG PREVIEW</Link>
+      <div className="border border-blue-400 p-1.5 text-blue-400">
+        {HEADER_MENU.map((menu) => {
+          return <Link href={menu.url}>{menu.title}</Link>;
+        })}
+      </div>
     </header>
   );
 }
